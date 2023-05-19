d3f6d0ea52cfe355c00f21317b049f1588a5045a
Blackteaxx <211820073@smail.nju.edu.cn>
2023-05-19 22:21:34 +0800

    Upd

--- 网页设计/js/shell.js
+++
@@ -1,9 +0,0 @@
-function openShell() {
-    document.getElementById("myShell").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeShell() {
-    document.getElementById("myShell").style.width = "0px";
-    document.getElementById("main").style.marginLeft = "0px";
-}
